97faef73266ca9b5ec81374165ecffb16e4c988b
rasanathpt <rasaananooru@gmail.com>
2022-11-30 19:01:07 +0530

    newv1

--- youtube/user/urls.py
+++ youtube/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.user_home)
+    path('',views.user_home),
 ]
--- youtube/youtube/settings.py
+++ youtube/youtube/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'youtube.urls'
@@ -119,13 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-     os.path.join(BASE-DIR,'user/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'user/static'),
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
